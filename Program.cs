af4a62b2ec39d595f924b892b6da6fd70b466405
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-14 11:49:08 +0530

    AllUC

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AssignmentDay34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO Employee PayRoll Services");
+            EmployeeRepository employeeRepository = new EmployeeRepository();
+            employeeRepository.GetDataFromSql();
+        }
+    }
+}
